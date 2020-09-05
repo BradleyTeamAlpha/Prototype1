c6b41073f2063e5f3f4a2444ecffbe03d51268e4
Doug Guzman <swiftshadow34@gmail.com>
2020-09-05 15:59:32 -0500

    Adds tile swapping and 2 tile prefabs

---
+++ Assets/Scripts/Conveyors/ConveyorManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorManager : MonoBehaviour
+{
+    
+    [Tooltip("Tiles the player has currently selected")]
+    public List<GameObject> selectedTiles;
+    
+    
+    /*
+     * if list is length 2
+     *     swap tiles
+     *
+     * if list is length 1 and button is hit
+     *     do thing (rotate, undo etc...)
+     */
+
+    private void Update()
+    {
+        if (selectedTiles.Count == 2)
+        {
+            Vector3 temp = selectedTiles[0].transform.position;
+
+            selectedTiles[0].transform.position = selectedTiles[1].transform.position;
+            selectedTiles[1].transform.position = temp;
+            
+            selectedTiles.Clear();
+        }
+    }
+}
---
+++ Assets/Scripts/Conveyors/ConveyorSelector.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConveyorSelector : MonoBehaviour
+{
+
+    public ConveyorManager manager;
+
+    private void Start()
+    {
+        manager = GameObject.FindWithTag("TileManager").GetComponent<ConveyorManager>();
+    }
+    
+    private void OnMouseOver()
+    {
+        Debug.Log($"Mousing over {gameObject}");
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log($"Mouse down on {gameObject}");
+        manager.selectedTiles.Add(gameObject);
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log($"Mouse exit {gameObject}");
+    }
+}
