5d19adb9f317aa496d92b2401eb192241147bbd6
Doug Guzman <swiftshadow34@gmail.com>
2020-09-08 15:29:45 -0500

    Adds feedback for selecting and hovering over tiles

--- Assets/Scripts/Conveyors/ConveyorManager.cs
+++ Assets/Scripts/Conveyors/ConveyorManager.cs
@@ -7,7 +7,12 @@ public class ConveyorManager : MonoBehaviour
     
     [Tooltip("Tiles the player has currently selected")]
     public List<GameObject> selectedTiles;
-    
+
+    [Tooltip("What color selected tiles should be")]
+    public Color selectedColor = new Color(0, 0.5f, 0);
+
+    [Tooltip("How many swaps the player has done")]
+    public int swapsDone = 0;
     
     /*
      * if list is length 2
@@ -19,13 +24,26 @@ public class ConveyorManager : MonoBehaviour
 
     private void Update()
     {
+        foreach (var tile in selectedTiles)
+        {
+            SpriteRenderer sr = tile.GetComponent<SpriteRenderer>();
+            sr.color = selectedColor;
+        }
+        
         if (selectedTiles.Count == 2)
         {
-            Vector3 temp = selectedTiles[0].transform.position;
+            if (!selectedTiles[0].Equals(selectedTiles[1]))
+            {
+                Vector3 temp = selectedTiles[0].transform.position;
 
-            selectedTiles[0].transform.position = selectedTiles[1].transform.position;
-            selectedTiles[1].transform.position = temp;
-            
+                selectedTiles[0].transform.position = selectedTiles[1].transform.position;
+                selectedTiles[1].transform.position = temp;
+                selectedTiles[1].GetComponent<SpriteRenderer>().color = selectedTiles[1].GetComponent<ConveyorSelector>().startColor;
+                ++swapsDone;
+            }
+
+            selectedTiles[0].GetComponent<SpriteRenderer>().color = selectedTiles[0].GetComponent<ConveyorSelector>().startColor;
+
             selectedTiles.Clear();
         }
     }
--- Assets/Scripts/Conveyors/ConveyorSelector.cs
+++ Assets/Scripts/Conveyors/ConveyorSelector.cs
@@ -6,26 +6,50 @@ using UnityEngine;
 public class ConveyorSelector : MonoBehaviour
 {
 
+    [Tooltip("Reference to the player manager")]
     public ConveyorManager manager;
 
+
+    /// <summary>
+    /// Reference to the object's sprite renderer
+    /// </summary>
+    private SpriteRenderer sr;
+    
+    [Tooltip("The starting color of the object")]
+    public Color startColor;
+    
     private void Start()
     {
         manager = GameObject.FindWithTag("TileManager").GetComponent<ConveyorManager>();
+        sr = GetComponent<SpriteRenderer>();
+        startColor = sr.color;
     }
     
-    private void OnMouseOver()
+    /// <summary>
+    /// Darkens the tile on mouse over
+    /// </summary>
+    private void OnMouseEnter()
     {
-        Debug.Log($"Mousing over {gameObject}");
+        Color newColor = sr.color;
+        newColor.r -= 0.5f;
+        newColor.g -= 0.5f;
+        newColor.b -= 0.5f;
+        sr.color = newColor;
     }
 
+    /// <summary>
+    /// Adds the selected tile to the selected list
+    /// </summary>
     private void OnMouseDown()
     {
-        Debug.Log($"Mouse down on {gameObject}");
         manager.selectedTiles.Add(gameObject);
     }
 
+    /// <summary>
+    /// Resets the color on mouse exit
+    /// </summary>
     private void OnMouseExit()
     {
-        Debug.Log($"Mouse exit {gameObject}");
+        sr.color = startColor;
     }
 }
