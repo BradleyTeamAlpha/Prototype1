28a3d80f9b0a8e2937f5c22558dc218c757344fa
Doug Guzman <swiftshadow34@gmail.com>
2020-09-10 15:35:27 -0500

    Adds a delay to the coal checking and moving its direction

--- Assets/Scripts/Conveyors/ConveyorMovement.cs
+++ Assets/Scripts/Conveyors/ConveyorMovement.cs
@@ -1,51 +1,55 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ConveyorMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        //objects will move themselves; on collider enter, move forward x long at x speed
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private ConveyorDirection cd;
     void OnTriggerStay2D(Collider2D movement)
     {
         //transform.Translate(Vector2.up * Time.deltaTime);
-        ConveyorDirection cd = movement.gameObject.GetComponent<ConveyorDirection>();
-        //Debug.Log(cd.shifter);
-        switch (cd.shifter)
-        {
-            
-            case ConveyorDirection.bitShiftMayhem.Left:
-                transform.Translate(Vector2.left * Time.deltaTime);
-                Debug.Log("going left");
-                break;
-            case ConveyorDirection.bitShiftMayhem.Right:
-                transform.Translate(Vector2.right * Time.deltaTime);
-                Debug.Log("going right");
-                break;
-            case ConveyorDirection.bitShiftMayhem.Up:
-                transform.Translate(Vector2.up * Time.deltaTime);
-                Debug.Log("going up");
-                break;
-            case ConveyorDirection.bitShiftMayhem.Down:
-                transform.Translate(Vector2.down * Time.deltaTime);
-                Debug.Log("going down");
-                break;
-
-            default:
-                break;
-
-
-        }
         
+        if (cd != null)
+        {
+            switch (cd.shifter)
+            {
+
+                case ConveyorDirection.bitShiftMayhem.Left:
+                    transform.Translate(Vector2.left * Time.deltaTime);
+                    Debug.Log("going left");
+                    break;
+                case ConveyorDirection.bitShiftMayhem.Right:
+                    transform.Translate(Vector2.right * Time.deltaTime);
+                    Debug.Log("going right");
+                    break;
+                case ConveyorDirection.bitShiftMayhem.Up:
+                    transform.Translate(Vector2.up * Time.deltaTime);
+                    Debug.Log("going up");
+                    break;
+                case ConveyorDirection.bitShiftMayhem.Down:
+                    transform.Translate(Vector2.down * Time.deltaTime);
+                    Debug.Log("going down");
+                    break;
+
+                default:
+                    break;
+
+
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(SetDirection(other.gameObject));
+    }
+
+    private IEnumerator SetDirection(GameObject target)
+    {
+        yield return new WaitForSeconds(0.9f);
+        cd = target.GetComponent<ConveyorDirection>();
+        transform.position *= 1.001f;
     }
 }
