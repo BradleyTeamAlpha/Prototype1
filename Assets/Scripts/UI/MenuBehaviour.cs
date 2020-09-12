26ce1bbc593a15b66d12a91cec2ae7189507e59c
darlynn-best <70346167+darlynn-best@users.noreply.github.com>
2020-09-11 21:03:51 -0500

    a couple menu things but I'm not done

---
+++ Assets/Scripts/UI/MenuBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour
+{
+    int level = 0;
+    public float waitTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// This function loads a scene.
+    /// </summary>
+    /// <param name="levelName">The int that is entered is
+    /// the scene that is loaded.</param>
+    public void LoadLevel(int levelName)
+    {
+        level = levelName;
+        Invoke("LoadNext", waitTime);
+    }
+
+    public void LoadNext()
+    {
+        // Loads the next scene.
+        UnityEngine.SceneManagement.SceneManager.LoadScene(level);
+    }
+}
