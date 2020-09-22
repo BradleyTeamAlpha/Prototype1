ca9b92f87e15204ea2ed09db420e76558e8b84e7
QDonnelly-254 <qdonnelly@mail.bradley.edu>
2020-09-22 15:02:23 -0500

    Prototype T Tile
    We've made a prototype T-tile. Just an FYI, it looks like the straight rainbow tile.

---
+++ Assets/Scripts/Conveyors/MultiExitTiles.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiExitTiles : MonoBehaviour
+{
+   private ConveyorDirection select;
+   void Start()
+    {
+        select = gameObject.GetComponent<ConveyorDirection>();//cache the ConveyorDirection
+
+    }
+    uint RotateLeft(uint x, int y)
+    {
+        //(original << bits) | (original >> (32 - bits))
+        uint k = (x << y);
+        if (k > 8)
+        {
+            k = 1;
+        }
+        return k;
+    }
+    uint RotateRight(uint x, int y)
+    {
+        //(original >> bits) | (original << (32 - bits))
+        uint k = (x >> y);
+        if (k < 1)
+        {
+            k = 8;
+        }
+        return k;
+    }
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        uint x = (uint)select.direction;
+        x = RotateLeft(x, 2);
+        select.direction = (ConveyorDirection.Direction)x;
+        Debug.Log("This is being called");
+    }
+}
